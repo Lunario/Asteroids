b582bf14c9bd8a2f39dd4f84c6d8d872a59e5181
Lunario <Domjekel@hotmail.com>
2025-07-15 21:45:20 +0200

    Added Update function to Player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import * 
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -10,14 +11,25 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    playerInst = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    
     while True:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        
 
+        playerInst.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        playerInst.update(dt)
+
 
 
 if __name__ == "__main__":
